37db06bf49dc76a8963320e2c03ee1c20bbf2f8c
timophe91 <74023447+timophe91@users.noreply.github.com>
2021-03-23 20:52:28 +0100

    3

--- bash/start.sh
+++ bash/start.sh
@@ -4,11 +4,14 @@ sudo apt install gedit
 mkdir -p ~/ctf
 touch ~/ctf/t
 touch ~/ctf/e
+touch ~/ctf/runAll.sh
 cp ../python4fun/* ~/ctf/
 cp getHosts.sh ~/ctf/
 cp nm.sh ~/ctf/
 cp findString.sh ~/ctf/
 cd ~/ctf
+echo "python3 ./runAll.py t e" > runAll.sh
+chmod +x runAll.sh
 rm *.txt
 chmod +x *.exp
 chmod +x *.sh
